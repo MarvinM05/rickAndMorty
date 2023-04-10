2172cbba90e8919431d216b95f00dd22281cc41e
Marvin Méndez Muñoz <m.mendez@teletica.com>
2023-04-09 22:28:07 -0600

    nuevo loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ const Loader = () => {
     
     return (
         <div className="loader">
-            <img className="pikle" src={`/public/pikle-rick.png`} alt="" />
+            <img className="pikle" src={`/public/rickandmorty.png`} alt="" />
         </div>
     )
 }
